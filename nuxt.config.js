a8e6dc9179325a54df7c4c98d3d74cee38526880
rafal-mac <maciejewskirafal99@gmail.com>
2020-11-28 12:10:18 +0000

    change global font family to Poppins

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,11 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap'
+      }
     ]
   },
   /*
@@ -22,8 +26,7 @@ export default {
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: ['~/assets/styles.css'],
   /*
   ** Plugins to load before mounting the App
   */
