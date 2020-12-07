3c96353691ed4fb14f52284f1097f846defb84e9
rafal-mac <maciejewskirafal99@gmail.com>
2020-12-07 00:06:42 +0000

    add contact me section

--- components/organisms/ContactMeSection/ContactMeSection.vue
+++ components/organisms/ContactMeSection/ContactMeSection.vue
@@ -1,34 +1,61 @@
 <template>
   <div class="fullscreen-contact-me d-flex">
     <div class="m-auto">
-      <div class="form">
-        <div class="d-flex position-relative experience-title-wrapper">
-          <p class="experience-title mr-auto mr-sm-0 ml-sm-auto">
-            Let's work together<span class="dot">.</span>
+      <div class="d-flex position-relative contact-me-section-title-wrapper mb-4">
+        <p class="contact-me-section-title mr-auto mr-sm-0 ml-sm-auto">
+          Let's work together<span class="dot">.</span>
+        </p>
+      </div>
+      <div class="form-wrapper d-flex flex-column flex-lg-row p-5">
+        <div class="d-flex flex-column pl-lg-4">
+          <p class="contact-me-title">
+            Contact me
           </p>
+          <p class="contact-me-text">
+            Send me a message and I will get back to you within 24 hours. (give or take)
+          </p>
+          <div class="my-4 my-lg-auto">
+            <b-button class="mb-2" variant="outline-primary" size="lg" href="tel:+447545133154">
+              <b-img-lazy class="mr-2" src="../../../assets/phone.svg" alt="phone" height="15px" />
+              +44 7545 133154
+            </b-button>
+            <b-button class="mb-2" variant="outline-primary" size="lg" href="mailto:maciejewskirafal99@gmail.com">
+              <b-img-lazy class="mr-2" src="../../../assets/email.svg" alt="email" height="15px" />
+              maciejewskirafal99@gmail.com
+            </b-button>
+          </div>
+          <div class="mx-auto mx-lg-0 mb-4 mt-auto">
+            <a href="https://www.linkedin.com/in/rafa%C5%82-maciejewski-908260136/">
+              <b-img-lazy class="social-icon" src="../../../assets/linkedin.svg" alt="linkedin" height="50px" />
+            </a>
+            <a href="https://twitter.com/heblushed">
+              <b-img-lazy class="social-icon" src="../../../assets/twitter.svg" alt="twitter" height="50px" />
+            </a>
+            <a href="https://www.instagram.com/serowy_toast/">
+              <b-img-lazy class="social-icon" src="../../../assets/instagram.svg" alt="instagram" height="50px" />
+            </a>
+          </div>
         </div>
-        <form name="contact" method="POST" data-netlify="true">
-          <p>
-            <label>Your Name: <b-input type="text" name="name" /></label>
-          </p>
-          <p>
-            <label>Your Email: <b-input type="email" name="email" /></label>
-          </p>
-          <p>
-            <label>Your Role: <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message" /></label>
-          </p>
-          <p>
-            <button type="submit">
-              Send
-            </button>
-          </p>
-        </form>
+        <div class="form p-4 ml-auto">
+          <form name="contact" method="POST" data-netlify="true">
+            <p>
+              <b-label>Your Name<b-input class="mt-2" type="text" name="name" /></b-label>
+            </p>
+            <p>
+              <b-label>Email<b-input class="mt-2" type="email" name="email" /></b-label>
+            </p>
+            <p>
+              <b-label>Message<br> <b-textarea class="mt-2" size="lg" rows="6" name="message" no-resize /></b-label>
+            </p>
+            <p>
+              <b-button variant="primary" type="submit">
+                <div class="my-2 mx-3">
+                  Send message
+                </div>
+              </b-button>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -43,10 +70,58 @@ export default class ContactMeSection extends Vue {
 }
 </script>
 <style scoped>
-.form {
-  background-color: #007bff;
+.social-icon {
+  transition: transform 0.3s;
+  padding: 10px;
+  border-radius: 10%;
+}
+
+.social-icon:hover {
+  transform: scale(1.3);
+  background-color: #027BFC;
+}
+
+input, textarea{
+    background-color: #f0f0f2;
+    border: none;
+}
+
+.contact-me-title {
+  font-size: 2.5rem;
+  color: white
+}
+
+.contact-me-text {
+  color: #999AB6;
+  width: 100%;
+}
+@media only screen and (min-width: 992px) {
+  .contact-me-text {
+    width: 70%;
+  }
+}
+
+.form-wrapper {
+  background-color: #01044A;
   border-radius: 20px;
 }
+@media only screen and (min-width: 992px) {
+  .form-wrapper {
+    width: 1000px;
+  }
+}
+
+.form {
+  background-color: white;
+  border-radius: 20px;
+  width: 100%;
+}
+@media only screen and (min-width: 992px) {
+  .form {
+    width: 45%;
+  }
+}
+
 
 .fullscreen-contact-me {
   height: 100vh;
@@ -56,17 +131,17 @@ export default class ContactMeSection extends Vue {
   color: #007bff;
 }
 
-.experience-title-wrapper {
+.contact-me-section-title-wrapper {
   font-size: 2.5rem;
   width: 100%;
 }
 @media only screen and (min-width: 1200px) {
-  .experience-title-wrapper {
+  .contact-me-section-title-wrapper {
     width: 80%;
     font-size: 5rem;
   }
 }
-.experience-title::before {
+.contact-me-section-title::before {
     content: "";
     position: absolute;
     right: 0;
@@ -76,7 +151,7 @@ export default class ContactMeSection extends Vue {
     background: #007bff;
 }
 @media only screen and (max-width: 575px) {
-  .experience-title::before {
+  .contact-me-section-title::before {
     content: "";
     position: absolute;
     left: 0;
